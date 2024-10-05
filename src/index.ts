2c910e23464ce9453c5e6fd172ab8f6a5f85e212
tuphan-dn <tuphan@descartes.network>
2024-10-05 17:57:35 +0700

    fix: remove consensus

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { keys } from '@libp2p/crypto'
-import { bytesToHex, hexToBytes } from 'ethereum-cryptography/utils'
+import { hexToBytes } from 'ethereum-cryptography/utils'
 
 import { PRIVATE_KEY } from './config'
 import Swarm from './swarm'
@@ -11,11 +11,6 @@ async function main() {
   const { swarm } = await Swarm.new(privkey)
   await swarm.services.dht.setMode('server')
   await swarm.start()
-  const topic = 'consensus'
-  swarm.services.pubsub.subscribe(topic)
-  swarm.services.pubsub.addEventListener('message', async ({ detail }) => {
-    console.info('Message:', detail.topic, bytesToHex(detail.data))
-  })
   // Bridge
   const bridge = new Bridge('0x10C3Cd012657a3DC886c203B9d7363A33BA73AAA')
   bridge.watch()
